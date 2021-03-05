a69a147f5b9e9e5e2ddc08858d610121faa2d9e7
qkrwjdtn1236 <48826426+qkrwjdtn1236@users.noreply.github.com>
2021-03-05 12:08:51 +0900

    Initial commit

---
+++ Assets/NewBehaviourScript1.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player
+{
+    private string name;
+    private int level = 1;
+
+    public Player(string name)
+    {
+        this.name = name;
+    }
+    
+    public void PlayerInfo()
+    {
+        Debug.Log("이름 : " + this.name);
+        Debug.Log("레벨 : " + this.level);
+    }
+
+    public void LevelUp()
+    {
+        this.level++;
+        Debug.Log("레벨 업");
+    }
+    public void SetName(string name)
+    {
+        this.name = name;
+        Debug.Log("이름을 변경했습니다 (변경 : " + this.name + ")");
+    }
+    public void SetLevel(int level)
+    {
+        this.level = level;
+        Debug.Log("레벨 변경했습니다 (변경 : "+this.level+")");
+    }
+}
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player myPlayer = new Player("박정수");
+        myPlayer.PlayerInfo();
+        myPlayer.LevelUp();
+        myPlayer.PlayerInfo();
+    }
+
+
+}
